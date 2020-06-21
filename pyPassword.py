25cd5af8c171f6c77e03e3b28af6f199bac1566c
Philippe Miron <philippemiron@gmail.com>
2020-06-20 21:28:29 -0400

    updated to python3

--- pyPassword.py
+++ pyPassword.py
@@ -12,6 +12,12 @@ import os
 import string
 import time
 
+#binascii.hexlify(data)¶
+#Return the hexadecimal representation of the binary data. Every byte of data is converted into the corresponding 2-digit hex representation. The resulting string is therefore twice as long as the length of data.
+
+#binascii.unhexlify(hexstr)
+#Return the binary data represented by the hexadecimal string hexstr. This function is the inverse of b2a_hex(). hexstr must contain an even number of hexadecimal digits (which can be upper or lower case), otherwise a TypeError is raised.
+# 
 class pypassword:
   def __init__(self, file, blockSize):
     self.file = file
@@ -49,8 +55,9 @@ class pypassword:
     return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
 
   # remove padding and return text
+  # if last byte is n, remove n bytes
   def unpad(self, s):
-    return s[:-ord(s[len(s)-1:])]
+    return s[:-s[-1]]
 
   def validHmac(self, hmac):
     with open(self.file) as f:
@@ -82,95 +89,83 @@ class pypassword:
       f.write(self.hmac + "\n")
       if self.websites is not None:
         for site in self.websites:
-          f.write(self.encrypt(site) + " " + self.encrypt(self.websites[site][0]) + " " + self.encrypt(self.websites[site][1]) + "\n")
+          site_user = self.websites[site][0]
+          site_pass = self.websites[site][1]
+          f.write('%s %s %s\n' %(self.encrypt(site), self.encrypt(site_user), self.encrypt(site_pass)))
 
   # login information
   # generate key and hmac from username/password
   def prompt(self):
-    username = raw_input("username: ")
+    username = input("username: ")
     password = getpass('password: ')
-    # generate key from username/password
-    key = PBKDF2(password, SHA512.new(username).hexdigest(), dkLen=32, count=20000)
-    # hmac
-    hmac = HMAC.new(key, msg=username, digestmod=SHA512).hexdigest()
+    
+    # key: password + salted with the SHA512(username)
+    # hmac: key and username 
+    # the hmac will be used to authentification and is stored on top of the secret file
+    key = PBKDF2(password, SHA512.new(username.encode('utf-8')).hexdigest(), dkLen=32, count=20000)
+    hmac = HMAC.new(key, msg=username.encode('utf-8'), digestmod=SHA512).hexdigest()
     return (key, hmac)
 
   def encrypt(self, text):
-    padded = self.pad(text).encode("hex")
+    padded = self.pad(text).encode()
     iv = Random.new().read(self.bs)
-    cipher = AES.new(self.key.decode("hex"), AES.MODE_CBC, iv)
-    return iv.encode("hex") + cipher.encrypt(padded.decode("hex")).encode("hex")
+    cipher = AES.new(self.key, AES.MODE_CBC, iv)
+    return iv.hex() + cipher.encrypt(padded).hex()
   
   def decrypt(self, text):
-    iv = text[:2*self.bs].decode("hex")
-    ciphertext = text[2*self.bs:].decode("hex")
-    cipher = AES.new(self.key.decode("hex"), AES.MODE_CBC, iv)
+    iv = bytes.fromhex(text[:2*self.bs])
+    ciphertext = bytes.fromhex(text[2*self.bs:])
+    cipher = AES.new(self.key, AES.MODE_CBC, iv)
     message = cipher.decrypt(ciphertext)
-    return self.unpad(message)
+    return self.unpad(message).decode()
 
-  # add a website username/password
   def addWebsite(self):
     self.viewList()
-    url = raw_input("\nwebsite url: ")
-    username = raw_input("username: ")
-    password = raw_input("password: ")
+    url = input("\nwebsite url: ")
+    username = input("username: ")
+    password = input("password: ")
     self.websites[url.lower()] = [username, password]
     self.writeData()
   
-  # remove a website username/password
   def remWebsite(self):
     self.viewList()
-    url = raw_input("\nwebsite url: ")
+    url = input("\nwebsite url: ")
     if url in self.websites.keys():
-      print("password removed for " + url)
+      print("Data removed for " + url)
       del self.websites[url]
       self.writeData()
     else:
-      print("No password for " + url)
+      print("No data for %s." % url)
   
-  # modify a website username/password
   def modWebsite(self):
     self.viewList()
-    url = raw_input("\nwebsite url: ")
+    url = input("\nwebsite url: ")
     if url in self.websites.keys():
-      username = raw_input("username: ")
-      password = raw_input("password: ")
+      username = input("username: ")
+      password = input("password: ")
       self.websites[url] = [username, password]
       self.writeData()
     else:
-      print("No password for " + url)
+      print("No data for %s." % url)
       
   # search for a password
   def getWebsite(self):
     self.viewList()
-    url = raw_input("\nwebsite url: ")
-    if url in self.websites.keys():
+    url = input("\nwebsite url: ")
+    try:
       [username, password] = self.websites[url]
       print("username: "  + username + "\npassword: " + password)
-    else:
-      print("No password for " + url)
+    except:
+      print("No data for %s." % url)
   
   # print list of password
   def viewList(self):
     self.clearConsole()
     if self.websites is not None:
       print("URL List:")
-      urls = self.websites.keys()
-      for url in urls:
+      for url in self.websites.keys():
         print(url)
   
-  # generate a password
-  # Not used in current implementation
-  def genpassword(self):
-    length = raw_input("number of characters [30]: ")
-    if length == "":
-      length = 30
-    else:
-      length = int(length)
-    alphabet = "".join((string.letters, string.digits, string.punctuation))
-    pw = "".join(random.choice(alphabet) for _ in range(length))
-    print(pw)
-
 # Set file path here
 file = "./secret.txt"
 
@@ -182,28 +177,28 @@ obj.clearConsole()
 
 # Check if file exist
 if not os.path.isfile(file) or os.stat(file).st_size == 0:
-  print "Create a new master username/password"
+  print("Create a new master username/password")
   obj.key, obj.hmac = obj.prompt()
   obj.writeData()
 else:
-  # authenticate and parse
+  # authenticate and parse the password file
   while n < 3:
     key, hmac = obj.prompt()    
     # validate username/password
     if obj.validHmac(hmac):
-      obj.key = key.encode('hex')
+      obj.key = key #.encode('hex')
       obj.parseData()
       break
     else:
       print("wrong username/password combinations.")
       time.sleep(1)
-      n = n + 1
+      n += 1
 
 # Menu and options
 ans = True
 while ans and n<3:
   obj.showMenu()
-  ans=raw_input("> ")
+  ans=input("> ")
   obj.clearConsole()
   
   if ans=="a":
@@ -219,9 +214,10 @@ while ans and n<3:
     obj.modWebsite()
   
   elif ans=="r":
-    obj.websites = None
-    obj.writeData()
-    print("Resetting file.")
+    if input("Reset the database? (y/n [n])") == 'y':
+      obj.websites = None
+      obj.writeData()
+      print("Database resetted.")
   
   elif ans=="v":
     obj.viewList()
